c61ead02c06e0318d2b756a2c1d0cd790a5c720e
Mansuang Pawong <mansaung@hotmnail.com>
2021-06-12 15:33:59 +0700

    update getWeb3

--- helpers/getWeb3.js
+++ helpers/getWeb3.js
@@ -23,13 +23,10 @@ const getWeb3 = () =>
       }
       // Fallback to localhost; use dev console port by default...
       else {
-        // const provider = new Web3.providers.HttpProvider(
-        //   "http://127.0.0.1:8545"
-        // );
         console.log("No web3 instance injected, using Local web3.");
         const provider = new HDWalletProvider(
-          "myself expand horn click column stay still glide lock private song world", 
-          "http://127.0.0.1:7545", 
+          process.env.MNEMONIC, 
+          process.env.URI, 
           AccountIndex);
         const web3 = new Web3(provider);
         
