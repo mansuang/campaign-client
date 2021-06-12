dce03f9f4ef7b9e35a3ee46a4e5195ae8369aa22
Mansuang Pawong <mansaung@hotmnail.com>
2021-06-12 15:38:33 +0700

    update nuxt.config.js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    ['@nuxtjs/dotenv', {filename: "../.env"}]
+    ['@nuxtjs/dotenv', {filename: "./.env"}]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
